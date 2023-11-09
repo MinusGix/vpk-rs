08393ffc5ea2a60aba9eaa701afad91db8380913
MinusGix <MinusGix@gmail.com>
2023-11-09 16:49:43 -0600

    Just comment out the examples
    I'm not sharing the library atm much so examples are less useful and
    they are more 'extract and list files' which doesn't work quite as
    neatly with how I set things up. Definitely still implementable but not
    relevant for what I'm doing.

--- examples/extract.rs
+++ examples/extract.rs
@@ -1,31 +1,31 @@
-extern crate vpk;
+// extern crate vpk;
 
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
-use std::vec::Vec;
+// use std::env;
+// use std::fs;
+// use std::fs::File;
+// use std::io::Write;
+// use std::path::Path;
+// use std::vec::Vec;
 
-use vpk::vpk::ProbableKind;
+// use vpk::vpk::ProbableKind;
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<_> = env::args().collect();
+    // let args: Vec<_> = env::args().collect();
 
-    if args.len() != 3 {
-        panic!("Usage: extract <path to vpk_dir.vpk> <path to export dir>");
-    }
+    // if args.len() != 3 {
+    //     panic!("Usage: extract <path to vpk_dir.vpk> <path to export dir>");
+    // }
 
-    // Check destination dir
-    let path = Path::new(&args[2]);
-    if !path.is_dir() {
-        panic!("Given export path is not directory or doesn't exists");
-    }
+    // // Check destination dir
+    // let path = Path::new(&args[2]);
+    // if !path.is_dir() {
+    //     panic!("Given export path is not directory or doesn't exists");
+    // }
 
-    let vpk_file = match vpk::from_path(&args[1], ProbableKind::None) {
-        Err(e) => panic!("Error while open file {}, err {}", &args[1], e),
-        Ok(vpk_file) => vpk_file,
-    };
+    // let vpk_file = match vpk::from_path(&args[1], ProbableKind::None) {
+    //     Err(e) => panic!("Error while open file {}, err {}", &args[1], e),
+    //     Ok(vpk_file) => vpk_file,
+    // };
 
     // TODO: fix this
     // for (file, vpk_entry) in vpk_file.tree.iter() {
--- examples/list.rs
+++ examples/list.rs
@@ -1,20 +1,20 @@
-extern crate vpk;
+// extern crate vpk;
 
-use std::env;
+// use std::env;
 
-use vpk::vpk::ProbableKind;
+// use vpk::vpk::ProbableKind;
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    // let args: Vec<_> = env::args().collect();
 
-    if args.len() == 1 {
-        panic!("Input file is not specified");
-    }
+    // if args.len() == 1 {
+    //     panic!("Input file is not specified");
+    // }
 
-    let vpk_file = match vpk::from_path(&args[1], ProbableKind::None) {
-        Err(e) => panic!("Error while open file {}, err {}", &args[1], e),
-        Ok(vpk_file) => vpk_file,
-    };
+    // let vpk_file = match vpk::from_path(&args[1], ProbableKind::None) {
+    //     Err(e) => panic!("Error while open file {}, err {}", &args[1], e),
+    //     Ok(vpk_file) => vpk_file,
+    // };
 
     // TODO: fix this
     // for (file, _) in &vpk_file.tree {
